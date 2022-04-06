f47e240ad1dd4b69ab38fccf7f05ceda34a91f49
dev11 <dev11@dev11s-MacBook-Pro.local>
2022-04-06 10:05:01 -0500

    able to pull from server

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  urlBase: '/Users/dev11/Desktop/services_pedboxV4',
-  // urlBase: '/root/www/avinal/src/',
+  // urlBase: '/Users/dev11/Desktop/services_pedboxV4',
+  urlBase: '/root/www/service_pedboxV4/service_pedboxV4',
 };
 
 module.exports = config;
